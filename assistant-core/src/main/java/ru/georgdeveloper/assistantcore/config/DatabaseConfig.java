31e80a0cbbcdb656bc9508fb437a3f020764e0ad
GeorgDeveloper <ifreyd@yandex.ru>
2025-10-21 14:55:33 +0300

    исправление ошибок

--- assistant-core/src/main/java/ru/georgdeveloper/assistantcore/config/DatabaseConfig.java
+++ assistant-core/src/main/java/ru/georgdeveloper/assistantcore/config/DatabaseConfig.java
@@ -3,6 +3,7 @@ package ru.georgdeveloper.assistantcore.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,6 +16,7 @@ import com.zaxxer.hikari.HikariDataSource;
  */
 @Configuration
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "ru.georgdeveloper.assistantcore.repository")
 public class DatabaseConfig {
 
     @Value("${data-sync.sql_server.url}")
@@ -29,8 +31,39 @@ public class DatabaseConfig {
     @Value("${data-sync.sql_server.driver}")
     private String sqlServerDriver;
 
+    @Value("${spring.datasource.url}")
+    private String mysqlUrl;
+    
+    @Value("${spring.datasource.username}")
+    private String mysqlUsername;
+    
+    @Value("${spring.datasource.password}")
+    private String mysqlPassword;
+    
+    @Value("${spring.datasource.driver-class-name}")
+    private String mysqlDriver;
+
     /**
-     * Источник данных для SQL Server
+     * Основной источник данных для MySQL (для JPA сущностей)
+     */
+    @Bean(name = "dataSource")
+    @Primary
+    public DataSource dataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(mysqlUrl);
+        ds.setUsername(mysqlUsername);
+        ds.setPassword(mysqlPassword);
+        ds.setDriverClassName(mysqlDriver);
+        ds.setMinimumIdle(1);
+        ds.setMaximumPoolSize(10);
+        ds.setIdleTimeout(60000);
+        ds.setConnectionTimeout(30000);
+        ds.setPoolName("mysql-hikari");
+        return ds;
+    }
+
+    /**
+     * Источник данных для SQL Server (для прямых запросов)
      */
     @Bean(name = "sqlServerDataSource")
     public DataSource sqlServerDataSource() {
@@ -45,6 +78,15 @@ public class DatabaseConfig {
         ds.setConnectionTimeout(30000);
         ds.setPoolName("sqlserver-hikari");
         return ds;
+    }
+
+    /**
+     * JdbcTemplate для работы с MySQL
+     */
+    @Bean(name = "jdbcTemplate")
+    @Primary
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 
     /**
